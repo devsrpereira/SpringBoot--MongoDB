29db5058839b834b89b4e8456cbb103c796660d2
devsrpereira <srpereira@uni9.edu.br>
2023-05-29 22:15:41 -0300

    operacao de instanciacao da base de dados

---
+++ src/main/java/com/srdevpereira/springbootmongodb/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.srdevpereira.springbootmongodb.config;
+
+import com.srdevpereira.springbootmongodb.domain.User;
+import com.srdevpereira.springbootmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User u1 = new User("1", "Maria Silva", "maria@gmail.com");
+        User u2 = new User("2", "Alex Monteiro", "alex@gmail.com");
+        User u3 = new User("3", "Saulo Pereira", "saulo@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(u1, u2, u3));
+    }
+}
