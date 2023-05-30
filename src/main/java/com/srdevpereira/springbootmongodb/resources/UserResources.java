382029c98ed95c5a60acd982f5f501167dc1f92b
devsrpereira <srpereira@uni9.edu.br>
2023-05-30 17:18:29 -0300

    Deletar usuario com delete

--- src/main/java/com/srdevpereira/springbootmongodb/resources/UserResources.java
+++ src/main/java/com/srdevpereira/springbootmongodb/resources/UserResources.java
@@ -35,7 +35,7 @@ public class UserResources {
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
         User obj = service.fromDTO(objDTO);
         obj = service.insert(obj);
@@ -43,4 +43,11 @@ public class UserResources {
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
