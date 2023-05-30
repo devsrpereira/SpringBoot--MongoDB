b555b0fd3e68f6dd20d6c367c7518af7e7086b6f
devsrpereira <srpereira@uni9.edu.br>
2023-05-30 19:04:42 -0300

    Referenciando os posts do usuario

--- src/main/java/com/srdevpereira/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/srdevpereira/springbootmongodb/config/Instantiation.java
@@ -43,5 +43,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("23/03/2023"),"Bom dia!","Acordei feliz hoje!", new AuthorDTO(u1));
         postRepository.saveAll(Arrays.asList(post1, post2));
 
+        u1.getPosts().addAll(Arrays.asList(post1,post2));
+        userRepository.save(u1);
+
     }
 }
